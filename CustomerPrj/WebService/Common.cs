b9891724fb62a75c23342c0b5f4f7f0a65806a5a
isaac <cjohnxiao@gmail.com>
2022-09-16 17:55:12 +0800

    (requirements 2) try from SortedSet to ImmutableSortedSet for the sake of thread safe

---
+++ CustomerPrj/WebService/Common.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Immutable;
+using System.Diagnostics;
+
+public static class COMMON
+    {
+    private const string init_file = "../samples.txt";
+    public static SortedSet<Rank> LeaderBoard =new SortedSet<Rank>();
+    //public static ImmutableSortedSet<Rank> LeaderBoard { get; set; } = ImmutableSortedSet.Create<Rank>();
+    
+    public static void InitSamples()
+        {
+        Console.WriteLine("Init some data to do test, see samples.txt\n初始化一点点数据便于测试，见samples.txt");
+        using (StreamReader customer = new StreamReader(init_file))
+            {
+            while (!customer.EndOfStream)
+                {
+                string[] item = customer.ReadLine().Split('\t');
+                long customer_id = long.Parse(item[0]);
+                var score = decimal.Parse(item[1]);
+                LeaderBoard.Add(new Rank(score, customer_id));
+                }
+            }
+        }
+}
---
+++ CustomerPrj/WebService/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class CustomerController: ControllerBase
+    {
+    //需求1
+    [HttpPost]
+    public Customer? UpdateScore(long id, long score)
+        {
+        //post请求回显不出来
+        return new Customer(id, score + 100, 1);
+        }
+    //需求2
+    [HttpGet]
+    public IEnumerable<Customer> GetCustomerByRank(int start,int end)
+        {
+        IEnumerable<Rank> res = COMMON.LeaderBoard.Skip(start-1);
+        long rank = start;
+
+        foreach (var customer in res)
+            {
+            if (rank <= end)
+                yield return new Customer(customer.customer_id_, customer.score_, rank++);
+            }
+        }
+    //需求3
+    [HttpGet]
+    public Customer? GetCustomerById(long id,long low, long high)
+        {
+        //能返回客户端回显出来就意味着在代码里拿到这3个数字了
+        return new Customer(id, low,high);
+        }
+    }
+
